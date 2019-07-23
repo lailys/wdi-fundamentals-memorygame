85683615806453e88d7bf9828a59b4dbde372d06
LailyS <laily@lailys.com>
2019-07-23 13:57:10 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,54 @@
 
-const cards=["queen","queen","king","king"]
+const cards=[
+{
+  rank:"queen"  ,
+ suit:"heart",
+ cardImage:"images/queen-of-hearts.png"
+  
+},
+{
+    rank:"queen"  ,
+   suit:"dimonds",
+   cardImage:"images/queen-of-diamonds.png"
+    
+  },
+  {
+    rank:"king"  ,
+   suit:"heart",
+   cardImage:"images/king-of-hearts.png"
+    
+  },
+  {
+    rank:"king"  ,
+   suit:"dimonds",
+   cardImage:"images/king-of-diamonds.png"
+    
+  },
+
+
+]
 const cardsInPlay=[]
+
+
+
 function checkForMatch(){
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-      } else {
-        console.log("Sorry, try again.");
-      }
-}
-function flipCard(cardId){
-    checkForMatch()
- console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId])
+
+ if(cardsInPlay.length>1){
+cardsInPlay[0]=== cardsInPlay[1]?alert("You found a match!"):alert("Sorry, try again.") 
+ }
+
 }
 
-flipCard(0);
-flipCard(2);
+
+function flipCard(cardId){
+   
+console.log("User flipped " + cards[cardId].rank)
+console.log("User flipped " + cards[cardId].cardImage)
+cardsInPlay.push(cards[cardId].rank)
+console.log(cardsInPlay)
+checkForMatch()
+
+}
+
+flipCard(0)
+flipCard(3)
